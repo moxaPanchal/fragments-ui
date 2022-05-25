89073853aa728debb30c4f5dc641e1df3481ad1e
moxaPanchal <moxaa.panchal@gmail.com>
2022-05-24 22:52:02 -0400

    Update api.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 // src/app.js
 import { Auth, getUser } from './auth';
+import { getUserFragments } from './api';
 
 async function init() {
   // Get our UI elements
@@ -38,6 +39,9 @@ async function init() {
 
   // Disable the Login button
   loginBtn.disabled = true;
+
+   // Do an authenticated request to the fragments API server and log the result
+   getUserFragments(user);
 }
 
 // Wait for the DOM to be ready, then start the app
